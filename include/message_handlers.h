1d41699c861ec5f7df7bd3423eecab9376c2251f
Rene van Oostrum <Rene.vanOostrum@amd.com>
2024-10-13 08:33:42 -0500

    Preparations for refactoring message handling on the host.

---
+++ include/message_handlers.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "message.h"
+
+namespace dh_comms
+{
+    class message_handler_base
+    {
+    public:
+        message_handler_base();
+        virtual ~message_handler_base() = 0;
+        virtual bool handle(const message_t& message) = 0;
+        virtual void merge_state(message_handler_base&&){};
+    };
+
+    class message_handlers_t {
+    public:
+        message_handlers_t(){};
+        ~message_handlers_t() = default;
+        message_handlers_t(const message_handlers_t&) = delete;
+        message_handlers_t& operator=(const message_handlers_t&) = delete;
+
+        bool handle(const message_t& message);
+        void add_handler(std::unique_ptr<message_handler_base>&& message_handler);
+
+    private:
+        std::vector<std::unique_ptr<message_handler_base>> message_handlers;
+    };
+} // namespace dh_comms
---
+++ src/message_handlers.cpp
@@ -0,0 +1,22 @@
+#include "message_handlers.h"
+
+namespace dh_comms
+{
+        bool message_handlers_t::handle(const message_t& message)
+        {
+            for(auto& mh: message_handlers)
+            {
+                if(mh->handle(message))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void message_handlers_t::add_handler(std::unique_ptr<message_handler_base>&& message_handler)
+        {
+            message_handlers.push_back(std::move(message_handler));
+        }
+
+} // namespace dh_comms
